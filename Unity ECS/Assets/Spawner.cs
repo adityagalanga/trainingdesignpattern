bea157c1c3a8cc3a89f76df8dd0f5a9fb5f84a23
Aditya Galang Adzana <adityagalang80@gmail.com>
2021-06-05 00:44:23 +0700

    Unity ECS part 3

--- Unity ECS/Assets/Spawner.cs
+++ Unity ECS/Assets/Spawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
-public class Spawner : IComponentData
+public struct Spawner : IComponentData
 {
     public Entity Prefab;
     public int Erows;
